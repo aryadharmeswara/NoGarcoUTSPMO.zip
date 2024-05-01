f39f52520b8a15d5960721316d55e8952fea1a58
aryadharmeswara <97155450+aryadharmeswara@users.noreply.github.com>
2024-05-01 08:42:38 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/pemesanan_no_antrian_gacoan_146733721/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pemesanan_no_antrian_gacoan_146733721
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+        import 'package:pemesanan_no_antrian_gacoan_146733721_f2f/figma_to_flutter.dart' as f2f;
+        
+void main() {
+
+    runApp(
+        f2f.getApp(
+            withInit: (){
+
+                print('Figma to Flutter initialized!');
+                f2f.subscribeToEvent('pageLoaded', (e) async {
+
+                    String pageName = e.payload;
+                    print('$pageName loaded');
+
+                });
+
+            }
+        )
+    );
+
+}
